44123fb193b9488fd23d8b2b0b8d775ea96c6224
VarudeAkash <myiitkdata@gmail.com>
2024-08-23 04:41:50 +0530

    Added project Images

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -6,13 +6,13 @@ function Projects() {
     {
       title: 'Intelligent Exercise Recognition and Counting Band with IMU Sensors',
       description: 'A brief description of Project 1',
-      image: '../../public/Images/2.png',  // Replace with actual image path
+      image: 'Images/2.png',  // Replace with actual image path
       githubLink: 'https://github.com/VarudeAkash/CS724-Sensing-Communications-and-Networking-for-Smart-Wireless-Devices/tree/main/Project',
     },
     {
       title: 'RoBERTa Enhanced MahaHate Classification',
       description: 'A brief description of Project 2.',
-      image: '/public/Images/2.png', // Replace with actual image path
+      image: 'Images/2.png', // Replace with actual image path
       githubLink: 'https://github.com/VarudeAkash/CS689-Computational-Linguistic-for-Indian-Languages/tree/main/Project',
     },
     {
@@ -24,19 +24,19 @@ function Projects() {
       {
         title: 'Advance Explorations in Machine Learning',
         description: 'A brief description of Project 4.',
-        image: '/Images/1.jpg',  // Replace with actual image path
+        image: 'Images/1.jpg',  // Replace with actual image path
         githubLink: 'https://github.com/VarudeAkash/CS771-Introduction-to-Machine-Learning/tree/main/project',
       },
       {
         title: 'Image Synthesis using Generative Adverserial Networks',
         description: 'A brief description of Project 5.',
-        image: '/src/components/Images/1.jpg',  // Replace with actual image path
+        image: 'Images/1.jpg',  // Replace with actual image path
         githubLink: 'https://github.com/VarudeAkash/Image-Synthesis-Using-GAN',
       },
       {
         title: 'Computational Linguistics in Indian Language',
         description: 'A brief description of Project 1.',
-        image: '/images/1.jpg',  // Replace with actual image path
+        image: 'Images/1.jpg',  // Replace with actual image path
         githubLink: 'https://github.com/VarudeAkash/CS689--Computational-Linguistic-for-Indian-Languages/tree/main/Projects',
       }
     // Add more projects here...
